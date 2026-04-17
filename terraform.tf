badbcd0a41af4160a5e61dcb5e1859f7ce0656a1
RupaEnggVille <rupa@enggville.com>
2026-04-17 11:04:12 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -28,7 +28,7 @@ terraform {
 
   backend "s3" {
     bucket         	   = "terra-eks12"
-    key              	   = "state/terraform.tfstate"
+    key              	 = "state/terraform.tfstate"
     region         	   = "us-east-1"
   }
 
